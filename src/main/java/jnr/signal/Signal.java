461bfe6c12a5d31dcb613958530e948271758fe7
Charles Oliver Nutter <headius@headius.com>
2012-12-05 16:33:09 -0600

    Move to using jnr-posix.

--- src/main/java/jnr/signal/Signal.java
+++ src/main/java/jnr/signal/Signal.java
@@ -1,39 +1,79 @@
 package jnr.signal;
 
+import jnr.posix.POSIX;
+import jnr.posix.POSIXFactory;
+
 public class Signal {
+    private final jnr.constants.platform.Signal signal;
+    private static final POSIX posix = POSIXFactory.getPOSIX();
+    
     public int getNumber() {
-        return nativeSignal.getNumber();
+        return signal.intValue();
     }
 
     public String getName() {
-        return nativeSignal.getName();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return nativeSignal.equals(obj);
+        return signal.name().substring("SIG".length());
     }
 
     @Override
     public int hashCode() {
-        return nativeSignal.hashCode();
+        int hash = 3;
+        hash = 73 * hash + this.getNumber();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Signal other = (Signal) obj;
+        if (this.getNumber() != other.getNumber()) {
+            return false;
+        }
+        if ((this.getName() == null) ? (other.getName() != null) : !this.getName().equals(other.getName())) {
+            return false;
+        }
+        return true;
     }
 
     public String toString() {
-        return nativeSignal.toString();
+        return "SIG" + getName();
     }
 
     public Signal(String name) {
-        nativeSignal = new NativeSignal(name);
+        signal = jnr.constants.platform.Signal.valueOf("SIG" + name);
+    }
+    
+    private static class POSIXSignalHandler implements jnr.posix.SignalHandler {
+        private final Signal signal;
+        private final SignalHandler handler;
+        
+        POSIXSignalHandler(Signal signal, SignalHandler handler) {
+            this.signal = signal;
+            this.handler = handler;
+        }
+        
+        public void handle(int sig) {
+            assert signal.getNumber() == sig;
+            handler.handle(signal);
+        }
     }
 
     public static synchronized SignalHandler handle(final Signal signal, final SignalHandler sh) throws IllegalArgumentException {
-        return NativeSignal.handle(signal, sh);
+        jnr.posix.SignalHandler oldHandler = posix.signal(signal.signal, new POSIXSignalHandler(signal, sh));
+        
+        if (oldHandler instanceof POSIXSignalHandler) {
+            return ((POSIXSignalHandler)oldHandler).handler;
+        } else {
+            return null;
+        }
     }
 
     public static void raise(Signal signal) throws IllegalArgumentException {
-        NativeSignal.raise(signal);
+        posix.kill(posix.getpid(), signal.getNumber());
     }
-    
-    private final NativeSignal nativeSignal;
 }
